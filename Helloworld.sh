dfdc8bb895dbfe7750dbed59ed3b20e794c198e3
Robert Gillespie <gillespier523@gmail.com>
2023-04-26 02:48:00 +0000

    Hello world commit

---
+++ Helloworld.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script Name:                 Hello World
+# Author:                       Robert Gillespie
+# Date of latest revision:      4/25/2023
+# Purpose:                     To learn how to use git bash
+
+# Declaration of variables
+variable="I am working perfectly!"
+
+# Declaration of functions
+
+
+# Main
+
+echo " Hello World"
+echo $variable
+
+# End
+
+
+
