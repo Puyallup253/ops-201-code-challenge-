f7f74db6dadac8676047bb240bdc8e25405a301c
Robert Gillespie <gillespier523@gmail.com>
2023-04-27 01:05:59 +0000

    This is a login history

---
+++ ops201codechallenge3.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script Name:                 code challenge 3
+# Author:                       Robert Gillespie
+# Date of latest revision:      4/26/2023
+# Purpose:                     To learn about coding functions
+
+# Declaration of variables
+
+
+# Declaration of functions
+print_something () {
+    lastlog
+  echo "This is a login history"
+}
+
+# Main
+print_something
+print_something
+print_something
+
+# End
+
+
+
+
+
+
+print_something
+print_something
