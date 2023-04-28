39d52a00522dd3ec21b6a4bb4f029ea26dcb300d
Robert Gillespie <gillespier523@gmail.com>
2023-04-28 04:19:09 +0000

    Challenge 4

---
+++ ops201challenge4.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Script Name:                 code challenge 4
+# Author:                       Robert Gillespie
+# Date of latest revision:      4/27/2023
+# Purpose:                     To make new folders and create files inside each folder using an array. 
+
+# Declaration of variables
+tomdirArr=(dir1 dir2 dir3 dir4)
+
+# Declaration of functions
+    
+
+
+# Main
+mkdir dir1
+mkdir dir2
+mkdir dir3
+mkdir dir4
+touch /home/robert93/ops-201-code-challenge-/${tomdirArr[0]}/file.txt
+touch /home/robert93/ops-201-code-challenge-/${tomdirArr[1]}/file.txt
+touch /home/robert93/ops-201-code-challenge-/${tomdirArr[2]}/file.txt
+touch /home/robert93/ops-201-code-challenge-/${tomdirArr[3]}/file.txt
+
+
+
+# End
+
+
+
+
+
+
