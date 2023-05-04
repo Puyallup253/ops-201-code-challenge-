4d42315227a832ef334b95584c569575013034ce
Robert Gillespie <gillespier523@gmail.com>
2023-05-04 04:16:12 +0000

    challenge 8

---
+++ ops201challenge8.bat
@@ -0,0 +1,11 @@
+@REM Script Name:                 Hello World
+@REM Author:                       Robert Gillespie
+@REM Date of latest revision:      5/3/2023
+@REM Purpose:                     To copy one file to another drive as a backup
+
+@REM Main
+robocopy "C:\Users\j.thompson\Desktop\My Project File" "E:\My Project Files"
+@REM End
+
+
+
